6e86fad0bd284500d1331961511cbd72ffee97fe
Irwin-S <irwin.r.singh@gmail.com>
2020-08-11 15:11:21 -0600

    Added operator information

--- Program.cs
+++ Program.cs
@@ -6,17 +6,51 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-           double leftOperand, rightOperand, result;
+          // > - Greater Than
+          // < - Less Than
+          // >= - Greater than or Equal too
+          // <= - Less than or Equal too
+          // == -  Equal To
+          // != - Not Equal To
 
-           Console.WriteLine("Please enter the left operand: ");
-           leftOperand = int.Parse(Console.ReadLine());
-
-           Console.WriteLine("Please enter the right operand: ");
-           rightOperand = int.Parse(Console.ReadLine());
-
-           result = leftOperand / rightOperand;
+          // && - And (True if both are true)
+          // || - Or (True if one is true)
         
-           Console.WriteLine("The result of "+leftOperand+ " / "+rightOperand+ " is: "+result);
+        // Example 1
+          //Console.WriteLine(5 < 10);
+        
+        // Example 2
+          // Console.WriteLine( 4 > 2 && (5 * 4 == 22 || 8 / 3 == 2));
+          // 4 > 2 && True
+          // True && True
+          // True
+
+        // Example 3
+          // int age;
+
+          // Console.WriteLine("Please enter your age: ");
+          // age = int.Parse(Console.ReadLine());
+
+          // if (age >= 18)
+          {
+            // Console.WriteLine("You are legally allowed to gample in alberta");
+          }
+
+          // else
+          {
+            // Console.WriteLine("Sorry, please come back next year to try again");
+          }
+        // Example 4
+        // string password;
+
+        // Console.WriteLine("Please enter your password: ");
+        // password = Console.ReadLine();
+
+        // if (password == "Hello123")
+        {
+            // Console.WriteLine("Your password is correct!");
+        }
+
         }
     }
 }

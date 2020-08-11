461bf856e4841deb5370026d66f131d61918bd6c
Irwin-S <irwin.r.singh@gmail.com>
2020-08-11 10:32:43 -0600

    Added variable types and added age and birthyear calculations

--- Program.cs
+++ Program.cs
@@ -6,12 +6,27 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            
-            string name; // "Declare" (create) a variable using the datatype followed by the name.
+            // "Declare" (create) a variable using the datatype followed by the name.
+            string text; // Strings store text.
+            char character; // Stores a single character (inlcuding space, newline, etc.)
+            int whileNumber; // Int stores a number without a decimal point (positive or negative)
+            // byte, short and long are other integer types with varying size limits.
+            double number; // Double stores a number with a decimal point.
+            // float and ceimal are alternatives with quirks. Google them if you plan to use them.
+            bool boolean; // bool stores a true or false value.
 
-            name = "Irwin"; // Assign a value to the variable
+            string name; // Declare our variable.
+            int age, birthYear;
+
+            Console.WriteLine("Please enter your name below:"); // Tell the user what we're expecting.
+            name = Console.ReadLine(); // Assign a value to the variable.
+
+            Console.WriteLine("Please enter your age below:");
+            age = int.Parse(Console.ReadLine());
+
+            birthYear = 2020 - age;
             
-            Console.WriteLine(name); // Retrieve the value from the variable and send it to the console.
+            Console.WriteLine("Hello, " + name + "."); // Retrieve the value from the variable and send it to the console.
         }
     }
 }

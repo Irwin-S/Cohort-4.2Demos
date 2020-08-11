10b7b0fd24465ff314508b291018762589d36c1f
Irwin-S <irwin.r.singh@gmail.com>
2020-08-11 11:15:53 -0600

    Calculator example;

--- Program.cs
+++ Program.cs
@@ -6,27 +6,17 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            // "Declare" (create) a variable using the datatype followed by the name.
-            string text; // Strings store text.
-            char character; // Stores a single character (inlcuding space, newline, etc.)
-            int whileNumber; // Int stores a number without a decimal point (positive or negative)
-            // byte, short and long are other integer types with varying size limits.
-            double number; // Double stores a number with a decimal point.
-            // float and ceimal are alternatives with quirks. Google them if you plan to use them.
-            bool boolean; // bool stores a true or false value.
+           double leftOperand, rightOperand, result;
 
-            string name; // Declare our variable.
-            int age, birthYear;
+           Console.WriteLine("Please enter the left operand: ");
+           leftOperand = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter your name below:"); // Tell the user what we're expecting.
-            name = Console.ReadLine(); // Assign a value to the variable.
+           Console.WriteLine("Please enter the right operand: ");
+           rightOperand = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter your age below:");
-            age = int.Parse(Console.ReadLine());
-
-            birthYear = 2020 - age;
-            
-            Console.WriteLine("Hello, " + name + "."); // Retrieve the value from the variable and send it to the console.
+           result = leftOperand / rightOperand;
+        
+           Console.WriteLine("The result of "+leftOperand+ " / "+rightOperand+ " is: "+result);
         }
     }
 }

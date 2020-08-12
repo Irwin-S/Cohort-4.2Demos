0fef03bd16101009b04babbaf4c2e0d96b2717e3
Irwin-S <irwin.r.singh@gmail.com>
2020-08-12 12:01:43 -0600

    Added for loop, with counter adding and subtracting

--- Program.cs
+++ Program.cs
@@ -6,49 +6,21 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-          // > - Greater Than
-          // < - Less Than
-          // >= - Greater than or Equal too
-          // <= - Less than or Equal too
-          // == -  Equal To
-          // != - Not Equal To
+         string myString = "Hello, World!";
 
-          // && - And (True if both are true)
-          // || - Or (True if one is true)
-        
-        // Example 1
-          //Console.WriteLine(5 < 10);
-        
-        // Example 2
-          // Console.WriteLine( 4 > 2 && (5 * 4 == 22 || 8 / 3 == 2));
-          // 4 > 2 && True
-          // True && True
-          // True
+        //  int counter = 0;
+        //  while (counter < myString.Length)
+        //  {
+        //    Console.WriteLine($"String index {counter} is {myString[counter]}.");
+        //    counter++;
+           
+        //  } 
+        //   Console.WriteLine("Done");
 
-        // Example 3
-          // int age;
-
-          // Console.WriteLine("Please enter your age: ");
-          // age = int.Parse(Console.ReadLine());
-
-          // if (age >= 18)
-          {
-            // Console.WriteLine("You are legally allowed to gample in alberta");
-          }
-
-          // else
-          {
-            // Console.WriteLine("Sorry, please come back next year to try again");
-          }
-        // Example 4
-        // string password;
-
-        // Console.WriteLine("Please enter your password: ");
-        // password = Console.ReadLine();
-
-        // if (password == "Hello123")
+        // for (int i = 0; i < myString.Length; i++) //counting up
+        for (int i = myString.Length-1; i >= 0; i--) //counting down (note: For myString.Length-1, the -1 is required cause without it will be counting ouside as 'positon 13')
         {
-            // Console.WriteLine("Your password is correct!");
+          Console.WriteLine($"String index {i} is {myString[i]}.");
         }
 
         }
